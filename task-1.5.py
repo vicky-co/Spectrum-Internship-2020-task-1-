04c97d1b7df5fe5f08efbc7c74daf8f1f31c3d31
manas1410 <64586228+manas1410@users.noreply.github.com>
2020-05-18 20:50:52 +0530

    Update task-1.5.py

--- task-1.5.py
+++ task-1.5.py
@@ -4,5 +4,5 @@
 #S2={1,3,4,6,8,11,22,34,51,67}
 s1=set(map(int,input("Enter the Elements of Set1 each elements is separated by comma',':").split(",")))
 s2=set(map(int,input("Enter the Elements of Set2 each elements is separated by comma',':").split(",")))
-s1.update(s1.difference(s2))
+s1=s1.difference(s2)
 print(f"First set is updated with items that exist only in the first set and not in the second set: {s1}")
