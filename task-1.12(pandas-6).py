47b08b095c9960ba6a5bdc5b44e0d6f9826693ac
manas1410 <64586228+manas1410@users.noreply.github.com>
2020-05-19 14:05:35 +0530

    Update task-1.12(pandas-6).py

--- task-1.12(pandas-6).py
+++ task-1.12(pandas-6).py
@@ -1,7 +1,7 @@
 #task-1.12
 #Write a Pandas program to get sample 75% of the diamonds DataFrame's rows without replacement and store the remaining 25% of the rows in another DataFrame
 import pandas as pd
-loc="D:/college/Internship(Spectrum)/2/diamonds.csv"
+loc="https://raw.githubusercontent.com/manas1410/Spectrum-Internship-2020-task-1-/master/diamonds.csv"
 diamonds=pd.read_csv(loc)
 print("\nSample 75% of diamonds DataFrame's rows without replacement:")
 result = diamonds.sample(frac=0.75, random_state=99)
